c0ceeb9a7d6a616de563b4af0a3bc0d6c0f602cd
cuonghtph34430 <cuonghtph34430@fpt.edu.vn>
2023-11-01 15:13:35 +0700

    Initial commit

---
+++ app/src/main/java/cuonghtph34430/poly/ung_dung_nghe_nhac/MainActivity.java
@@ -0,0 +1,14 @@
+package cuonghtph34430.poly.ung_dung_nghe_nhac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
